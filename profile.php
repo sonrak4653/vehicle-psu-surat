06b9d52c792fd57ffca6b46ef055a0c58b11d6cb
blackdragon7841 <blackdragon7841@gmail.com>
2018-07-05 10:16:36 +0700

    change line user id

--- profile.php
+++ profile.php
@@ -3,8 +3,8 @@
 
 $access_token = 'ahbj8DK/OxNGHqr9fwgt4pNzw2TNhNIBtmbvEBw0ILtArrE0S78BE+FVulEvtk3y62bz7QkbT+J0f5MJRlC6yMRv4IQyMyTRQLEBV8s+UI/sdJ5YiKkhLB/mmgU8JhoIByJcUoH06CYb3hUotv6BFwdB04t89/1O/w1cDnyilFU=';
 
-//$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
-$userId = 'Ue8397db755c9256de543ed18d01dff28';
+
+$userId = 'U86d0fce03ebfd3cee4e221bbfc66db7c';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
