7c6db196de37ed76e6ec6cdbea8495d6b2a0ca77
develop <bumrungsak.s@hotmail.com>
2019-01-07 10:47:01 +0700

    change

--- profile.php
+++ profile.php
@@ -4,7 +4,8 @@
 $access_token = 'ahbj8DK/OxNGHqr9fwgt4pNzw2TNhNIBtmbvEBw0ILtArrE0S78BE+FVulEvtk3y62bz7QkbT+J0f5MJRlC6yMRv4IQyMyTRQLEBV8s+UI/sdJ5YiKkhLB/mmgU8JhoIByJcUoH06CYb3hUotv6BFwdB04t89/1O/w1cDnyilFU=';
 
 
-$userId = 'U86d0fce03ebfd3cee4e221bbfc66db7c';
+//$userId = 'U86d0fce03ebfd3cee4e221bbfc66db7c';
+$userId = 'Ue8397db755c9256de543ed18d01dff28';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
